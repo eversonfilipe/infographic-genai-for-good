feb2fb3ec33f8ddd572cd068807fd350d67e485b
Éverson Filipe <eversonfilipe124@gmail.com>
2025-07-21 00:00:12 -0300

    Update Index.tsx

--- src/Index.tsx
+++ src/Index.tsx
@@ -1,1 +1,12 @@
-.
+/**
+ * Ponto de entrada do app React
+ * Inclui CSS global e renderiza o <App />
+ */
+
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+import "./styles/global.css";
+
+const root = createRoot(document.getElementById("root")!);
+root.render(<React.StrictMode><App /></React.StrictMode>);
