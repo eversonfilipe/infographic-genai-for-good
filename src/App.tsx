9c7d71a87ee2122f1d672679df7fc4f0271cf263
Éverson Filipe <eversonfilipe124@gmail.com>
2025-07-20 23:58:33 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,1 +1,28 @@
-.
+/**
+ * App Principal do Hub de Informação GEN AI
+ * MVP escalável e responsivo, focado em jovens da Gen Z
+ * Comentado e documentado conforme melhores práticas de engenharia front-end
+ */
+
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import ArticlePage from "./pages/ArticlePage";
+import AboutPage from "./pages/AboutPage";
+import NotFoundPage from "./pages/NotFoundPage";
+import Navbar from "./components/Navbar";
+
+const App: React.FC = () => (
+  <Router>
+    {/* Barra de navegação fixa */}
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<HomePage />} />
+      <Route path="/artigo/:id" element={<ArticlePage />} />
+      <Route path="/sobre" element={<AboutPage />} />
+      <Route path="*" element={<NotFoundPage />} />
+    </Routes>
+  </Router>
+);
+
+export default App;
